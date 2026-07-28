570a72b60d5d685ef4b8eaf320e32ab70b495dcc
Lithiya V T <lithiya121994@gmail.com>
2026-07-28 09:18:34 +0530

    Add basic arithmetic operations to calculator

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,1 +1,11 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
 
+print("Addition:", n1 + n2)
+print("Subtraction:", n1 - n2)
+print("Multiplication:", n1 * n2)
+
+if n2 == 0:
+    print("Cannot divide by zero")
+else:
+    print("Division:", n1 / n2)
