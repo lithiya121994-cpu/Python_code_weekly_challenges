08653a17145557d144e22cd74c0c1a950448629c
Lithiya V T <lithiya121994@gmail.com>
2026-08-11 08:45:54 +0530

    Refactor sum of even numbers calculation

--- Sum_of_even_numbers_challenge.py
+++ Sum_of_even_numbers_challenge.py
@@ -1,10 +1,13 @@
 n=int(input("Enter a number:"))
-even_sum=0
-count=0
-for i in range(1,n+1):
-  if i%2==0:
-    even_sum+=i
-    count+=1
-print(" ")
-print("Sum of the evn numbers is:",even_sum)
-print("Number of even numbers:", count)
+if n>0:
+    even_sum = 0
+    count = 0
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            even_sum += i
+            count += 1
+    print(" ")
+    print("Sum of the evn numbers is:", even_sum)
+    print("Number of even numbers:", count)
+else:
+    print("Enter a number greater than 0")
